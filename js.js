cfa86b39b8aad351d546eca561ecfeba58b70329
CreeperHunter <Soccercraft@users.noreply.github.com>
2016-03-09 15:23:01 -1000

    Added files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+
+
+var press = function (){
+	document.getElementById('change').innerHTML = "<img src='SADNESS.png'>"
+};
+
+var reset = function(){
+	document.getElementById('change').innerHTML = "<input onclick='press()' type='image' id='button' src='UMAD.png'>"
+};
---
+++ script.js
@@ -0,0 +1,7 @@
+var audio = new Audio('neutre.mp3');
+function  neutre () {
+    audio.play();
+}
+function stopplaying () {
+   window.location.reload();
+}
